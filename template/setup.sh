e8216a30351dfb44ae90bb2b81722ebeba8a6714
Ray Hogenson <rayhogenson@openmailbox.org>
2017-04-11 13:42:30 -0400

    Move some things around for ease of maintenance
    I have a script setup.sh which copies around the files to make
    maintaining this website easy. I've written the script in pure POSIX sh,
    but it's a fragile script since I'm not good at shell scripting.
    Nevertheless, it serves its purpose so it's not worth rewriting.

---
+++ template/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/dash
+
+if [ -d template ]; then
+	cd template
+fi
+
+root=..
+template=$root/template
+pages=$template/pages
+posts=$template/posts
+raw=$template/raw
+
+# Blog
+cat $template/post_header.html > $pages/blog.html
+
+for file in $(ls $posts); do
+	echo '<h2>' >> $pages/blog.html
+	echo $file | sed -e 's/\.md//' -e 's/_/ /g' >> $pages/blog.html
+	echo '</h2>' >> $pages/blog.html
+	cmark $posts/$file >> $pages/blog.html
+done
+
+# Pages
+for file in $(ls $pages); do
+	sed -e "/{body}/{r $pages/$file
+d}" $template/template.html > $root/$file
+done
+
+# Raw
+for file in $(ls $raw); do
+	ln -srf -t $root $file $raw/$file
+done
