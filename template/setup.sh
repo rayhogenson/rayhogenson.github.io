732d56ef07c7757c37736ac182782c92c8a1299a
Ray Hogenson <rayhogenson@openmailbox.org>
2017-05-22 12:06:37 -0400

    Remove the blog
    I don't really want to update this and the posts currently up here are
    kind of embarrassing. They're still on Github for the world to see, of
    course, but it's not the kind of thing I want to spread around.

--- template/setup.sh
+++ template/setup.sh
@@ -7,18 +7,7 @@ fi
 root=..
 template=$root/template
 pages=$template/pages
-posts=$template/posts
 raw=$template/raw
-
-# Blog
-cat $template/post_header.html > $pages/blog.html
-
-for file in $(ls $posts | sort -r); do
-	echo '<h2>' >> $pages/blog.html
-	echo $file | sed -e 's/\.md//' -e 's/_/ /g' >> $pages/blog.html
-	echo '</h2>' >> $pages/blog.html
-	cmark $posts/$file >> $pages/blog.html
-done
 
 # Pages
 for file in $(ls $pages); do
